2597a5f519017f830ed0c78c8a97332aaa711ba3
devhunter1 <anubhabkundu2@gmail.com>
2024-12-04 18:49:09 +0530

    universal8535: build bluetooth HAL from hardware/samsung_slsi-linaro
    Signed-off-by: devhunter1 <anubhabkundu2@gmail.com>

--- common.mk
+++ common.mk
@@ -55,6 +55,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.a2dp@1.0-impl-exynos \
     android.hardware.bluetooth@1.1-impl \
     android.hardware.bluetooth@1.1-service \
     libbt-vendor
