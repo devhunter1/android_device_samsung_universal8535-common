1416e313f4cded8fd0a7aafb6230db9dcd58252c
devhunter1 <anubhabkundu2@gmail.com>
2024-12-07 20:24:32 +0530

    universal8535: working on sepolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -177,8 +177,6 @@ BOARD_SEPOLICY_TEE_FLAVOR := teegris
 include device/lineage/sepolicy/exynos/sepolicy.mk
 include device/samsung_slsi/sepolicy/sepolicy.mk
 
-BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
-BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
 
 # Vendor_boot
 BOARD_VENDOR_RAMDISK_FRAGMENTS := dlkm
