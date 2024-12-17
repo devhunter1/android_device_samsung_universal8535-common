fa4370fb66756d0b3f7f5e1efb151275aaf55927
santhosh6194 <santhosh6194@gmail.com>
2024-12-17 11:03:21 +0530

    universal8535-common: Build samsung doze

--- common.mk
+++ common.mk
@@ -119,6 +119,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
     
+
+# DOZE
+PRODUCT_PACKAGES += \
+    SamsungDoze
+    
 # Memtrack
 PRODUCT_PACKAGES += \
     android.hardware.memtrack-service.samsung-mali
