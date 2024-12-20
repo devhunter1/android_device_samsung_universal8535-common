1603a8ae3f60ef063e3396bfd91497480aa376da
devhunter1 <anubhabkundu2@gmail.com>
2024-12-20 12:01:02 +0530

    universal8535-common: change write_headers a14x

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "universal8535"
+write_headers "a14x"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
