1cacaea2141a5dd34d2e16e865f20e59ba7cd6b6
devhunter1 <anubhabkundu2@gmail.com>
2024-12-25 20:54:12 +0530

    universal8535-common: Include device compatibility matrix

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -173,6 +173,9 @@ TARGET_USES_MKE2FS := true
 # Manifest
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
 
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
