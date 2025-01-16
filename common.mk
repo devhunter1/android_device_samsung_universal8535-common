b9b0bd92c0293b96038c9e085c7ba7373e7b0842
devhunter1 <anubhabkundu2@gmail.com>
2025-01-16 18:38:17 +0530

    universal8535-common: audio impl: rebrand to universal8535

--- common.mk
+++ common.mk
@@ -26,7 +26,7 @@ PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := true
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@7.0-impl \
+    android.hardware.audio@7.0-impl_universal8535 \
     android.hardware.audio.service \
     android.hardware.audio.effect@7.0-impl \
     audio.bluetooth.default \
