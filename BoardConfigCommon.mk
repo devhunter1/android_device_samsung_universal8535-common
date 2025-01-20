5bbfbf2f7c7647a329bf2c47e87d0c38c5015e12
devhunter1 <anubhabkundu2@gmail.com>
2025-01-20 21:02:12 +0530

    universal8535-common: Include all vendor hal manifest to DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -173,7 +173,6 @@ TARGET_USES_MKE2FS := true
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml \
     $(COMMON_PATH)/configs/vintf/device_framework_matrix.xml
 
